82c3881b541a8ae6ee46c28cbbec398b7b719646
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 13:50:43 +0900

    feature : WinningLotto 클래스 및 일치하는 번호의 갯수 세는 기능 추가

---
+++ src/main/java/model/WinningLotto.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.Arrays;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonusNo;
+
+    public WinningLotto(Lotto lotto, int bonusNo) {
+        this.lotto = lotto;
+        this.bonusNo = bonusNo;
+    }
+
+    public Rank match(Lotto userLotto) {
+        int matchCount = getMatchCount(userLotto);
+        if (matchCount < 3) {
+            return Rank.NONE;
+        }
+        if (matchCount == 5 && userLotto.getNumbers().contains(bonusNo)) {
+            return Rank.SECOND;
+        }
+        if (matchCount == 5 && !userLotto.getNumbers().contains(bonusNo)) {
+            return Rank.THIRD;
+        }
+        return getRankByMatchCount(matchCount);
+    }
+
+    private int getMatchCount(Lotto userLotto) {
+        return (int) userLotto.getNumbers().stream()
+                .filter(number -> lotto.getNumbers().contains(number))
+                .count();
+    }
+
+    private Rank getRankByMatchCount(int matchCount) {
+        return Arrays.stream(Rank.values())
+                .filter(rank -> rank.getMatchCount() == matchCount)
+                .findFirst()
+                .orElse(Rank.NONE);
+    }
+
+}
