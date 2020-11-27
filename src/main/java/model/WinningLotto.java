32b7d67dce704c58279d782848be4761568d02d7
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 15:11:49 +0900

    feature : WinningLotto 객체 보너스 볼 검증 기능 추가

--- src/main/java/model/WinningLotto.java
+++ src/main/java/model/WinningLotto.java
@@ -3,12 +3,21 @@ package model;
 import java.util.Arrays;
 
 public class WinningLotto {
+    private static final String BONUS_NUMBER_IS_DUPLICATE = "보너스 번호가 중복됩니다.";
+
     private final Lotto lotto;
     private final int bonusNo;
 
     public WinningLotto(Lotto lotto, int bonusNo) {
+        validateBonusNo(lotto, bonusNo);
         this.lotto = lotto;
         this.bonusNo = bonusNo;
+    }
+
+    private void validateBonusNo(Lotto lotto, int bonusNo) {
+        if (lotto.getNumbers().contains(bonusNo)) {
+            throw new IllegalArgumentException(BONUS_NUMBER_IS_DUPLICATE);
+        }
     }
 
     public Rank match(Lotto userLotto) {
