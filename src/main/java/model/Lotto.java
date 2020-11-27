b5536aada48586049df1c087cc42d950fa512e7b
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 10:58:08 +0900

    refactor : Lotto 객체 상태값 검증 메서드 중복 검증 기능 추가 및 조건문 누락된 부분 수정

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -7,6 +7,7 @@ public class Lotto {
     private static final int LOTTO_SIZE = 6;
     private static final String LOTTO_NUMBERS_SIZE_MUST_BE_SIX = "로또 번호는 6개의 숫자여야 합니다.";
     private static final String LOTTO_NUMBERS_MUST_BETWEEN_ONE_AND_FOURTYFIVE = "로또 번호는 1부터 45 사이의 숫자로 구성되어야 합니다.";
+    private static final String LOTTO_NUMBERS_MUST_NOT_OVERLAP = "로또 번호는 중복된 번호가 존재하면 안됩니다.";
 
     private List<Integer> numbers;
 
@@ -19,10 +20,17 @@ public class Lotto {
         if (numbers.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(LOTTO_NUMBERS_SIZE_MUST_BE_SIX);
         }
-        if (numbers.stream().anyMatch(isBetweenOneAndFourtyFive)) {
+        if (numbers.stream().anyMatch(isBetweenOneAndFourtyFive.negate())) {
             throw new IllegalArgumentException(LOTTO_NUMBERS_MUST_BETWEEN_ONE_AND_FOURTYFIVE);
+        }
+        if (numbers.stream().distinct().count() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(LOTTO_NUMBERS_MUST_NOT_OVERLAP);
         }
     }
 
     private Predicate<Integer> isBetweenOneAndFourtyFive = (Integer number) -> 0 < number && number < 46;
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
