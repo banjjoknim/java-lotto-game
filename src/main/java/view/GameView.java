7945d399ad90229f4b2c02c448651ca8ce716831
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 15:00:23 +0900

    feature : 당첨번호 및 보너스 번호 입력기능 추가

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -2,13 +2,22 @@ package view;
 
 import model.Lotto;
 import model.LottoIssuer;
+import model.WinningLotto;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.util.stream.Collectors.toList;
 
 public class GameView {
     private static final String PLEASE_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String PURCHASED = "개를 구매했습니다.";
+    private static final String PLEASE_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String PLEASE_INPUT_BONUS_BALL = "보너스 볼을 입력해 주세요.";
+    private static final String WINNING_STATISTICS = "당첨 통계";
+    private static final String PARTIONING_LINE = "--------";
 
     private static Scanner sc = new Scanner(System.in);
 
@@ -20,11 +29,11 @@ public class GameView {
         return lottos;
     }
 
-    public static void printPurchased(List<Lotto> lottos) {
+    private static void printPurchased(List<Lotto> lottos) {
         System.out.println(lottos.size() + PURCHASED);
     }
 
-    public static int inputMoney() {
+    private static int inputMoney() {
         try {
             printPleaseInputMoney();
             String money = sc.nextLine();
@@ -35,14 +44,55 @@ public class GameView {
         }
     }
 
-    public static void printPleaseInputMoney() {
+    private static void printPleaseInputMoney() {
         System.out.println(PLEASE_INPUT_MONEY);
     }
 
-    public static void printLottos(List<Lotto> lottos) {
+    private static void printLottos(List<Lotto> lottos) {
         lottos.stream()
                 .map(Lotto::getNumbers)
                 .forEach(System.out::println);
+    }
+
+    public static WinningLotto inputWinningNumbersAndBonusNo() {
+        try {
+            List<Integer> winningNumbers = inputWinningNumbers();
+            int bonusNo = inputBonusNo();
+            return new WinningLotto(new Lotto(winningNumbers), bonusNo);
+        } catch (IllegalArgumentException e) {
+            return inputWinningNumbersAndBonusNo();
+        }
+    }
+
+    private static List<Integer> inputWinningNumbers() {
+        try {
+            printPleaseInputWinningNumbers();
+            String input = sc.nextLine();
+            return Arrays.stream(input.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .collect(toList());
+        } catch (NumberFormatException e) {
+            return inputWinningNumbers();
+        }
+    }
+
+    private static void printPleaseInputWinningNumbers() {
+        System.out.println(PLEASE_INPUT_WINNING_NUMBERS);
+    }
+
+    private static int inputBonusNo() {
+        try {
+            printPleaseInputBonusNo();
+            int bonusNo = sc.nextInt();
+            return bonusNo;
+        } catch (InputMismatchException e) {
+            return inputBonusNo();
+        }
+    }
+
+    private static void printPleaseInputBonusNo() {
+        System.out.println(PLEASE_INPUT_BONUS_BALL);
     }
 
 }
