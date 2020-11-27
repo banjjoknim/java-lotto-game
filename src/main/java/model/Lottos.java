af6a99c4fcc0d757eae76b3c21acc441c7c3f0c1
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 16:25:36 +0900

    feature : Lottos 클래스 및  통계 산출  기능 추가

---
+++ src/main/java/model/Lottos.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class Lottos {
+
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    public double calculateYield(WinningLotto winningLotto) {
+        Map<Rank, Long> statistics = getStatistics(winningLotto);
+        long benefit = calculateBenefit(statistics);
+        long payment = calculatePayment();
+        return (double) benefit / payment;
+    }
+
+    private long calculateBenefit(Map<Rank, Long> statistics) {
+        return statistics.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().getWinningMoney() * entry.getValue())
+                .sum();
+    }
+
+    private long calculatePayment() {
+        return lottos.size() * LottoIssuer.LOTTO_PRICE;
+    }
+
+    public Map<Rank, Long> getStatistics(WinningLotto winningLotto) {
+        return lottos.stream()
+                .collect(groupingBy((lotto -> winningLotto.match(lotto)), counting()));
+    }
+}
