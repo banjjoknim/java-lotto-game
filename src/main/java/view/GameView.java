71514ee90a9f0962e173836d5b2c29a9c39c4d9a
DESKTOP-FSO9NHB\User <zhfxmtkachdt@daum.net>
2020-11-27 16:27:23 +0900

    feature : 산출된 통계 출력 기능 추가

--- src/main/java/view/GameView.java
+++ src/main/java/view/GameView.java
@@ -1,23 +1,29 @@
 package view;
 
-import model.Lotto;
-import model.LottoIssuer;
-import model.WinningLotto;
+import model.*;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Collections.reverseOrder;
+import static java.util.stream.Collectors.*;
 
 public class GameView {
     private static final String PLEASE_INPUT_MONEY = "구입금액을 입력해 주세요.";
     private static final String PURCHASED = "개를 구매했습니다.";
     private static final String PLEASE_INPUT_WINNING_NUMBERS = "지난 주 당첨 번호를 입력해 주세요.";
+    private static final String PLEASE_INPUT_AGAIN = "잘못된 입력입니다. 다시 입력해 주세요.";
     private static final String PLEASE_INPUT_BONUS_BALL = "보너스 볼을 입력해 주세요.";
     private static final String WINNING_STATISTICS = "당첨 통계";
     private static final String PARTIONING_LINE = "--------";
+    private static final String GET_FIFTH_PLACE_COUNT_IS = "3개 일치 (5000원) - ";
+    private static final String GET_FOUTH_PLACE_COUNT_IS = "4개 일치 (50000원) - ";
+    private static final String GET_THIRD_PLACE_COUNT_IS = "5개 일치 (1500000원) - ";
+    private static final String GET_SECOND_PLACE_COUNT_IS = "5개 일치, 보너스 볼 일치 (30000000원) - ";
+    private static final String GET_FIRST_PLACE_COUNT_IS = "6개 일치 (2000000000원) - ";
+    private static final String COUNTS = "개";
+    private static final String TOTAL_YIELD_IS = "총 수익률은 ";
 
     private static Scanner sc = new Scanner(System.in);
 
@@ -60,6 +66,7 @@ public class GameView {
             int bonusNo = inputBonusNo();
             return new WinningLotto(new Lotto(winningNumbers), bonusNo);
         } catch (IllegalArgumentException e) {
+            printPleaseInputAgain();
             return inputWinningNumbersAndBonusNo();
         }
     }
@@ -87,12 +94,44 @@ public class GameView {
             int bonusNo = sc.nextInt();
             return bonusNo;
         } catch (InputMismatchException e) {
+            sc = new Scanner(System.in);
             return inputBonusNo();
         }
     }
 
     private static void printPleaseInputBonusNo() {
         System.out.println(PLEASE_INPUT_BONUS_BALL);
+    }
+
+    private static void printPleaseInputAgain() {
+        System.out.println(PLEASE_INPUT_AGAIN);
+    }
+
+    public static void printStatistics(WinningLotto winningLotto, Lottos lottos) {
+        System.out.println(WINNING_STATISTICS);
+        System.out.println(PARTIONING_LINE);
+
+        Map<Rank, Long> statistics = lottos.getStatistics(winningLotto);
+        printRankCount(statistics);
+        printYield(lottos.calculateYield(winningLotto));
+    }
+
+    private static void printRankCount(Map<Rank, Long> statistics) {
+        List<String> template = setStatisticsTemplate();
+        statistics.remove(Rank.NONE);
+        IntStream.range(0, template.size())
+                .mapToObj(index -> template.get(index) + statistics.getOrDefault(Rank.values()[index], 0L) + COUNTS)
+                .sorted(reverseOrder())
+                .forEach(System.out::println);
+    }
+
+    private static List<String> setStatisticsTemplate() {
+        return Stream.of(GET_FIRST_PLACE_COUNT_IS, GET_SECOND_PLACE_COUNT_IS, GET_THIRD_PLACE_COUNT_IS, GET_FOUTH_PLACE_COUNT_IS, GET_FIFTH_PLACE_COUNT_IS)
+                .collect(toList());
+    }
+
+    private static void printYield(double yield) {
+        System.out.println(TOTAL_YIELD_IS + yield + "입니다.");
     }
 
 }
