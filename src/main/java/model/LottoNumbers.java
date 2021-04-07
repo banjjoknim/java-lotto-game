0f96ff1c0f476d2564b91574c7e14a547136d2bb
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 03:34:22 +0900

    feat : LottoNumbers 클래스에 lottoNumbers 필드 및 생성자 추가

--- src/main/java/model/LottoNumbers.java
+++ src/main/java/model/LottoNumbers.java
@@ -10,6 +10,12 @@ public class LottoNumbers {
     private static final String LOTTO_NUMBERS_MUST_NOT_OVERLAP = "로또 번호는 중복된 번호가 존재하면 안됩니다.";
     public static final int LOTTO_NUMBERS_SIZE = 6;
 
+    private final List<LottoNumber> lottoNumbers;
+
+    public LottoNumbers(List<LottoNumber> lottoNumbers) {
+        this.lottoNumbers = lottoNumbers;
+    }
+
     public static List<Integer> createLottoNumbers() {
         try {
             List<Integer> lottoNumbers = createNumbers();
