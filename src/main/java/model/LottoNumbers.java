a88ae65a25b8e75d01aaeebc8f55581e4e3a7076
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 03:56:35 +0900

    refactor : Integer -> LottoNumber 로의 제네릭 타입 변경에 따른 LottoNumbers의 중복 여부 검증 로직 변경

--- src/main/java/model/LottoNumbers.java
+++ src/main/java/model/LottoNumbers.java
@@ -43,6 +43,7 @@ public class LottoNumbers {
 
     private static void validateOverlap(List<LottoNumber> lottoNumbers) {
         int distinctCount = (int) lottoNumbers.stream()
+                .mapToInt(lottoNumber -> lottoNumber.getNumber())
                 .distinct()
                 .count();
         if (distinctCount != LOTTO_NUMBERS_SIZE) {
