33c6548263fac13b33dc74fc40399e3ea981cb5c
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 12:50:57 +0900

    feat : LottoNumber 에 유효한 번호인지 검증 메서드 추가

--- src/main/java/model/LottoNumber.java
+++ src/main/java/model/LottoNumber.java
@@ -2,7 +2,10 @@ package model;
 
 public class LottoNumber {
     private static final int ZERO = 0;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
     private static final String LOTTO_NUMBER_MUST_BE_POSITIVE = "로또 번호는 양수여야 합니다.";
+    private static final String LOTTO_NUMBER_MUST_BETWEEN_ONE_AND_FORTY_FIVE = "로또 번호는 1과 45 사이의 숫자여야 합니다.";
 
     private final int number;
 
@@ -13,6 +16,12 @@ public class LottoNumber {
     private void validateNumberIsPositive(int number) {
         if (number < ZERO) {
             throw new IllegalArgumentException(LOTTO_NUMBER_MUST_BE_POSITIVE);
+        }
+    }
+
+    private void validateNumberIsValid(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_MUST_BETWEEN_ONE_AND_FORTY_FIVE);
         }
     }
 }
