4730c8159ae6ed2459da4b779ce45d015b8a3690
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 14:08:25 +0900

    feat : Lotto 클래스에 LottoNumbers 생성 메서드 추가

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -1,12 +1,24 @@
 package model;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Lotto {
+    private static final int LOTTO_SIZE = 6;
 
     private final List<LottoNumber> numbers;
 
     public Lotto(List<LottoNumber> numbers) {
         this.numbers = numbers;
     }
+
+    public static List<LottoNumber> generateLottoNumbers() {
+        return Stream.generate(LottoNumber::generateRandomNumber)
+                .limit(LOTTO_SIZE)
+                .map(LottoNumber::new)
+                .collect(toList());
+    }
+
 }
