1effd2f11e754c503da3c96e4037c6619ee0c295
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 16:14:32 +0900

    feat : WinningLotto 생성시 로또 번호와 보너스볼 번호 검증 메서드 추가 및 생성자에 로직 반영

--- src/main/java/model/WinningLotto.java
+++ src/main/java/model/WinningLotto.java
@@ -1,16 +1,26 @@
 package model;
 
 public class WinningLotto {
+    private static final String BONUS_BALL_NUMBER_IS_DUPLICATE = "보너스 볼 번호가 당첨 로또 번호와 중복됩니다.";
 
     private final Lotto lotto;
     private final int bonusNo;
 
     public WinningLotto(Lotto lotto, int bonusNo) {
+        validateBonusNo(lotto, bonusNo);
         this.lotto = lotto;
         this.bonusNo = bonusNo;
+    }
+
+    private void validateBonusNo(Lotto lotto, int bonusNo) {
+        LottoNumber bonusLottoNumber = new LottoNumber(bonusNo);
+        if (lotto.getNumbers().contains(bonusLottoNumber)) {
+            throw new IllegalArgumentException(BONUS_BALL_NUMBER_IS_DUPLICATE);
+        }
     }
 
     public Rank match(Lotto userLotto) {
         return null;
     }
+
 }
