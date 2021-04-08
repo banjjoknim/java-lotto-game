02dbd4bc39d9f94c0cfb305f5ccbb8fa308117b6
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 13:39:15 +0900

    Revert "refactor : LottoNumber 생성자 파라미터 제거"
    This reverts commit 6544b0191274975906357580587ec6ee3292b6b7.

--- src/main/java/model/LottoNumber.java
+++ src/main/java/model/LottoNumber.java
@@ -12,8 +12,7 @@ public class LottoNumber {
 
     private final int number;
 
-    public LottoNumber() {
-        int number = generateLottoNumber();
+    public LottoNumber(int number) {
         validateNumber(number);
         this.number = number;
     }
