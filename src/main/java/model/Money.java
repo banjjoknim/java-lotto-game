fe552dfd59a7a164a203782f2a8b8d615be2c4e9
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 15:42:44 +0900

    refactor : Money 클래스 필드 int -> BigDecimal로 변경

--- src/main/java/model/Money.java
+++ src/main/java/model/Money.java
@@ -1,22 +1,24 @@
 package model;
 
+import java.math.BigDecimal;
+
 public class Money {
     private static final String MONEY_MUST_BE_POSITIVE = "구입금액은 0 이상의 숫자여야 합니다.";
     private static final int ZERO = 0;
 
-    private final int money;
+    private final BigDecimal amount;
 
-    public Money(int money) {
-        validateMoney(money);
-        this.money = money;
+    public Money(BigDecimal amount) {
+        validateMoney(amount);
+        this.amount = amount;
     }
 
-    public int getMoney() {
-        return money;
+    public BigDecimal getAmount() {
+        return amount;
     }
 
-    private void validateMoney(int money) {
-        if (money < ZERO) {
+    private void validateMoney(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) < ZERO) {
             throw new IllegalArgumentException(MONEY_MUST_BE_POSITIVE);
         }
     }
