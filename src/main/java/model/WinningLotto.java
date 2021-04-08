7aaa5d10b9603d8423b6c08d77054f92fa384cbd
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 12:35:15 +0900

    feat : WinningLotto 클래스 추가

---
+++ src/main/java/model/WinningLotto.java
@@ -0,0 +1,16 @@
+package model;
+
+public class WinningLotto {
+
+    private final Lotto lotto;
+    private final int bonusNo;
+
+    public WinningLotto(Lotto lotto, int bonusNo) {
+        this.lotto = lotto;
+        this.bonusNo = bonusNo;
+    }
+
+    public Rank match(Lotto userLotto) {
+        return null;
+    }
+}
