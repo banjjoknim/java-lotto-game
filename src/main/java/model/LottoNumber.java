690575ab45534b751798358d5b9f399a7722307e
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 12:45:36 +0900

    feat : LottoNumber 에 양수 여부 검증 메서드 추가

--- src/main/java/model/LottoNumber.java
+++ src/main/java/model/LottoNumber.java
@@ -1,11 +1,18 @@
 package model;
 
 public class LottoNumber {
+    private static final int ZERO = 0;
+    private static final String LOTTO_NUMBER_MUST_BE_POSITIVE = "로또 번호는 양수여야 합니다.";
 
     private final int number;
 
     public LottoNumber(int number) {
         this.number = number;
     }
-    
+
+    private void validateNumberIsPositive(int number) {
+        if (number < ZERO) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_MUST_BE_POSITIVE);
+        }
+    }
 }
