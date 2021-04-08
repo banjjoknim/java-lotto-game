9742eec08c68cbe3269c586e171870ac7514d136
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-08 15:21:53 +0900

    feat : Money 클래스에 금액 검증 메서드 추가 및 Money 생성자에 해당 로직 추가

--- src/main/java/model/Money.java
+++ src/main/java/model/Money.java
@@ -1,11 +1,19 @@
 package model;
 
 public class Money {
+    private static final String MONEY_MUST_BE_POSITIVE = "구입금액은 0 이상의 숫자여야 합니다.";
+    private static final int ZERO = 0;
 
     private final int money;
 
     public Money(int money) {
+        validateMoney(money);
         this.money = money;
     }
-    
+
+    private void validateMoney(int money) {
+        if (money < ZERO) {
+            throw new IllegalArgumentException(MONEY_MUST_BE_POSITIVE);
+        }
+    }
 }
