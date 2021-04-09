02ff6712548319d4eb15165dc626100aa0a5c0c1
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-09 11:08:22 +0900

    feat : Rank 클래스에 맞춘 번호의 갯수와 보너스 번호 포함 유무를 통해 Rank를 결정하는 메서드 추가

--- src/main/java/model/Rank.java
+++ src/main/java/model/Rank.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public enum Rank {
     FIRST(6, 2000000000),
     SECOND(5, 30000000),
@@ -23,4 +26,16 @@ public enum Rank {
     public int getWinningMoney() {
         return winningMoney;
     }
+
+    public static Rank getRank(int matchCount, boolean hasBonusNo) {
+        ArrayList<Rank> ranks = new ArrayList<>(Arrays.asList(Rank.values()));
+        if (!hasBonusNo) {
+            ranks.remove(Rank.SECOND);
+        }
+        return ranks.stream()
+                .filter(rank -> rank.matchCount == matchCount)
+                .findFirst()
+                .orElseGet(() -> Rank.NONE);
+    }
+
 }
