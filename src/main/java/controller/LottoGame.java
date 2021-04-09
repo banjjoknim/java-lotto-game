4a9cc1c02ca67ca373b4028e73105974590fb1a5
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-09 15:39:45 +0900

    refactor : LottoGame에 있던 WinningLotto 생성 메서드 InputView 로 이관

--- src/main/java/controller/LottoGame.java
+++ src/main/java/controller/LottoGame.java
@@ -9,35 +9,15 @@ public class LottoGame {
     public static void main(String[] args) {
         OutputView.printPleaseInputPurchaseMoney();
         Money money = InputView.inputMoney();
-        Lottos lottos = LottoIssuer.issueLottos(money);
+        Lottos lottos = Lottos.issueLottos(money);
         OutputView.printHasPurchased(lottos);
         OutputView.printLottos(lottos);
 
-        Lotto lotto = setLottoByInput();
-        WinningLotto winningLotto = setWinningLottoByInput(lotto);
+        WinningLotto winningLotto = InputView.inputWinningLotto();
 
         OutputView.printWinningStatistics();
         OutputView.printSeparationLine();
         OutputView.printStatistics(lottos, winningLotto);
     }
 
-    private static WinningLotto setWinningLottoByInput(Lotto lotto) {
-        try {
-            OutputView.printPleaseInputBonusBallNumber();
-            String bonusBallNumber = InputView.inputBonusBallNumber();
-            return LottoIssuer.issueWinningLotto(lotto, new LottoNumber(Integer.valueOf(bonusBallNumber)));
-        } catch (IllegalArgumentException e) {
-            return setWinningLottoByInput(lotto);
-        }
-    }
-
-    private static Lotto setLottoByInput() {
-        try {
-            OutputView.printPleaseInputLastWeekWinningLottoNumber();
-            String lastWeekWinningLottoNumber = InputView.inputLastWeekWinningLottoNumber();
-            return LottoIssuer.generateLottoByInput(lastWeekWinningLottoNumber);
-        } catch (IllegalArgumentException e) {
-            return setLottoByInput();
-        }
-    }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,9 @@
 package view;
 
+import model.Lotto;
+import model.LottoNumber;
 import model.Money;
+import model.WinningLotto;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
@@ -14,11 +17,32 @@ public class InputView {
         return new Money(amount);
     }
 
-    public static String inputLastWeekWinningLottoNumber() {
+    public static WinningLotto inputWinningLotto() {
+        Lotto lotto = inputLotto();
+        try {
+            OutputView.printPleaseInputBonusBallNumber();
+            String bonusBallNumber = InputView.inputBonusNumber();
+            return WinningLotto.issueWinningLotto(lotto, new LottoNumber(Integer.valueOf(bonusBallNumber)));
+        } catch (IllegalArgumentException e) {
+            return inputWinningLotto();
+        }
+    }
+
+    private static String inputBonusNumber() {
         return scanner.next();
     }
 
-    public static String inputBonusBallNumber() {
+    public static Lotto inputLotto() {
+        try {
+            OutputView.printPleaseInputLastWeekWinningLottoNumber();
+            String lastWeekWinningLottoNumber = InputView.inputLastWeekWinningLottoNumber();
+            return Lotto.generateLottoByInput(lastWeekWinningLottoNumber);
+        } catch (IllegalArgumentException e) {
+            return inputLotto();
+        }
+    }
+
+    private static String inputLastWeekWinningLottoNumber() {
         return scanner.next();
     }
 
