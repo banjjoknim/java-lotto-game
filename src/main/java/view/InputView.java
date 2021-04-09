01d118d05ad3eb8b3f2b0c6cf55d5e0d0f36f7cc
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-09 13:23:39 +0900

    feat : InputView 클래스에 구입금액 입력 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,4 +1,17 @@
 package view;
 
+import model.Money;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static Money inputMoney() {
+        BigDecimal amount = new BigDecimal(scanner.next());
+        return new Money(amount);
+    }
+
 }
