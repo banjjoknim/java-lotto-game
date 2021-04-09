d41a06f022bb4f3e25e779f935c4271db30e29c4
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-09 14:06:19 +0900

    feat : Lottos 클래스에 WinningLotto와 대조하여 통계내는 메서드 추가

--- src/main/java/model/Lottos.java
+++ src/main/java/model/Lottos.java
@@ -1,9 +1,12 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
 
 public class Lottos {
+    private static final int INITIALIZED_STATISTICS_RANK_VALUE = 0;
+    private static final int ONE = 1;
 
     private List<Lotto> lottos;
 
@@ -13,5 +16,17 @@ public class Lottos {
 
     public List<Lotto> getLottos() {
         return new ArrayList<>(lottos);
+    }
+
+    public Map<Rank, Integer> getStatistics(WinningLotto winningLotto) {
+        Map<Rank, Integer> statistics = Arrays.stream(Rank.values())
+                .collect(toMap(rank -> rank, rank -> INITIALIZED_STATISTICS_RANK_VALUE));
+        lottos.forEach(lotto -> produceStatistics(winningLotto, statistics, lotto));
+        return statistics;
+    }
+
+    private void produceStatistics(WinningLotto winningLotto, Map<Rank, Integer> statistics, Lotto lotto) {
+        Rank rank = winningLotto.match(lotto);
+        statistics.put(rank, statistics.get(rank) + ONE);
     }
 }
