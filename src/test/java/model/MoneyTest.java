c51054e631532849d7bda240d726d23e2b120c0f
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-11 02:53:14 +0900

    test : 실제 사용 금액 계산 테스트 로직 변경

--- src/test/java/model/MoneyTest.java
+++ src/test/java/model/MoneyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -38,4 +39,19 @@ class MoneyTest {
         assertThat(money).isInstanceOf(Money.class);
         assertThat(money.getAmount()).isEqualTo(amount);
     }
+
+    @DisplayName("Money 객체에서 로또를 구매하는데 실제 사용된 금액 계산을 테스트 한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 1111, 2222, 5555, 10000, 24500})
+    void calculateTotalSpendMoneyTest(int amount) {
+        // given
+        Money money = new Money(new BigDecimal(amount));
+
+        // when
+        BigDecimal totalSpendMoney = money.calculateTotalSpendMoney();
+
+        // then
+        assertThat(totalSpendMoney).isEqualTo(new BigDecimal(amount).setScale(-3, RoundingMode.FLOOR));
+    }
+
 }
