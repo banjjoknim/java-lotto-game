b210d59dfcaf89a11937992f1e8c81cbfaba223d
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-10 22:10:52 +0900

    feat : LottoNumber 클래스 hashCode 오버라이드

--- src/main/java/model/LottoNumber.java
+++ src/main/java/model/LottoNumber.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class LottoNumber {
@@ -43,9 +44,16 @@ public class LottoNumber {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        LottoNumber lottoNumber = (LottoNumber) obj;
-        return this.getNumber() == lottoNumber.getNumber();
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
     }
-    
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
 }
