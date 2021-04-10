6156c5d7bf51abb7efb1ccaf707a5c8a2d8ed10a
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-11 02:14:26 +0900

    test : RankTest 에 @EnumSource 적용

--- src/test/java/model/RankTest.java
+++ src/test/java/model/RankTest.java
@@ -2,22 +2,22 @@ package model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
 
 class RankTest {
 
     @DisplayName("보너스볼을 포함할 때 matchCount 에 따른 랭크 결정을 테스트 한다.")
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6})
-    void getRankHasBonusNoTest(int matchCount) {
+    @EnumSource(value = Rank.class, names = {"THIRD"}, mode = EXCLUDE)
+    void getRankHasBonusNoTest(Rank rank) {
         // given
         boolean hasBonusNo = true;
-        Rank rank = setUpRank(matchCount, hasBonusNo);
 
         // when
-        Rank getRank = Rank.findMatchRank(matchCount, hasBonusNo);
+        Rank getRank = Rank.findMatchRank(rank.getMatchCount(), hasBonusNo);
 
         // then
         assertThat(getRank).isEqualTo(rank);
@@ -25,37 +25,16 @@ class RankTest {
 
     @DisplayName("보너스볼을 포함하지 않을 때 matchCount 에 따른 랭크 결정을 테스트 한다.")
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6})
-    void getRankHasNotBonusNoTest(int matchCount) {
+    @EnumSource(value = Rank.class, names = {"SECOND"}, mode = EXCLUDE)
+    void getRankHasNotBonusNoTest(Rank rank) {
         // given
         boolean hasBonusNo = false;
-        Rank rank = setUpRank(matchCount, hasBonusNo);
 
         // when
-        Rank getRank = Rank.findMatchRank(matchCount, hasBonusNo);
+        Rank getRank = Rank.findMatchRank(rank.getMatchCount(), hasBonusNo);
 
         // then
         assertThat(getRank).isEqualTo(rank);
-    }
-
-    private Rank setUpRank(int matchCount, boolean hasBonusNo) {
-        Rank rank = Rank.NONE;
-        if (matchCount == Rank.FIFTH.getMatchCount()) {
-            rank = Rank.FIFTH;
-        }
-        if (matchCount == Rank.FOURTH.getMatchCount()) {
-            rank = Rank.FOURTH;
-        }
-        if (matchCount == Rank.THIRD.getMatchCount()) {
-            rank = Rank.THIRD;
-        }
-        if (matchCount == Rank.SECOND.getMatchCount() && hasBonusNo) {
-            rank = Rank.SECOND;
-        }
-        if (matchCount == Rank.FIRST.getMatchCount()) {
-            rank = Rank.FIRST;
-        }
-        return rank;
     }
 
 }
