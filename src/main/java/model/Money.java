a62f510daca16e2a6ada8c186f7b25c82bda25e6
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-11 02:52:28 +0900

    refactor : Money의 실제 사용금액 계산 메서드의 잘못된 로직 변경

--- src/main/java/model/Money.java
+++ src/main/java/model/Money.java
@@ -1,12 +1,14 @@
 package model;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static model.Lotto.LOTTO_PRICE;
 
 public class Money {
     private static final String MONEY_MUST_BE_POSITIVE = "구입금액은 0 이상의 숫자여야 합니다.";
     private static final int IS_NEGATIVE = -1;
+    private static final int ROUNDING_SCALE = -3;
 
     private final BigDecimal amount;
 
@@ -28,7 +30,7 @@ public class Money {
     }
 
     public BigDecimal calculateTotalSpendMoney() {
-        return amount.divide(new BigDecimal(LOTTO_PRICE));
+        return amount.setScale(ROUNDING_SCALE, RoundingMode.FLOOR);
     }
 
     public BigDecimal getAmount() {
