905e5068908a54206748e40f5b463f17a30da50f
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-12 05:57:59 +0900

    feat : 유효하지 않은 로또 번호를 생성하는 전략 추가

---
+++ src/main/java/model/LessThanLottoMinNumberGenerator.java
@@ -0,0 +1,10 @@
+package model;
+
+public class LessThanLottoMinNumberGenerator implements NumberGenerator{
+
+    @Override
+    public int generate() {
+        return 0;
+    }
+
+}
---
+++ src/main/java/model/MoreThanLottoMaxNumberGenerator.java
@@ -0,0 +1,10 @@
+package model;
+
+public class MoreThanLottoMaxNumberGenerator implements NumberGenerator{
+
+    @Override
+    public int generate() {
+        return 46;
+    }
+
+}
