6711fdabc93f5d6c987741e9f316ca148fca2a0a
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-16 22:57:45 +0900

    style : public 상수 배치 변경, NumberGenerator 메서드 참조 적용

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -9,9 +9,10 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class Lotto {
+    public static final BigDecimal LOTTO_PRICE = new BigDecimal(1000);
     private static final int LOTTO_SIZE = 6;
     private static final String LOTTO_SIZE_MUST_BE_SIX = "로또 번호는 중복되지 않는 6개의 번호여야 합니다.";
-    public static final BigDecimal LOTTO_PRICE = new BigDecimal(1000);
+
 
     private final List<LottoNumber> numbers;
 
@@ -29,7 +30,7 @@ public class Lotto {
 
     public static Lotto generateLotto(NumberGenerator numberGenerator) {
         try {
-            List<LottoNumber> lottoNumbers = Stream.generate(() -> numberGenerator.generate())
+            List<LottoNumber> lottoNumbers = Stream.generate(numberGenerator::generate)
                     .map(LottoNumber::getLottoNumberFromCache)
                     .limit(LOTTO_SIZE)
                     .collect(toList());
