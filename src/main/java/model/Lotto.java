7cf07a576efc9f0587470116032f9bf76707f931
banjjoknim <zhfxmtkachdt@daum.net>
2021-04-20 12:05:15 +0900

    style : 불필요한 공백 라인 제거

--- src/main/java/model/Lotto.java
+++ src/main/java/model/Lotto.java
@@ -10,7 +10,6 @@ public class Lotto {
     public static final int LOTTO_SIZE = 6;
     private static final String LOTTO_SIZE_MUST_BE_SIX = "로또 번호는 중복되지 않는 6개의 번호여야 합니다.";
 
-
     private final List<LottoNumber> numbers;
 
     public Lotto(List<LottoNumber> numbers) {
